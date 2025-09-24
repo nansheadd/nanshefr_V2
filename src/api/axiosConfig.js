d0a7ce2a6f3504d0a1b8747da0cc22ee67fdcba2
nansheadd <nansheadd@gmail.com>
2025-09-25 00:33:02 +0200

    feat: add axios debug logging (#28)

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -8,7 +8,44 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
-// On a supprimé tout le bloc "apiClient.interceptors.request.use(...)"
-// car le navigateur gère maintenant le token pour nous.
+apiClient.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
+    console.debug('[API][request]', {
+      url: config.url,
+      method: config.method,
+      withCredentials: config.withCredentials,
+      cookies: document.cookie,
+      headers: config.headers,
+    });
+  }
+
+  return config;
+});
+
+apiClient.interceptors.response.use(
+  (response) => {
+    if (typeof window !== 'undefined') {
+      console.debug('[API][response]', {
+        url: response.config?.url,
+        status: response.status,
+        data: response.data,
+      });
+    }
+
+    return response;
+  },
+  (error) => {
+    if (typeof window !== 'undefined') {
+      console.error('[API][response][error]', {
+        url: error.config?.url,
+        status: error.response?.status,
+        data: error.response?.data,
+        headers: error.config?.headers,
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
